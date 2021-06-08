32e9d1d601013d6c7caa17852edd9ed12f409006
Michael McGuire <michael@mcguire.ch>
2021-06-08 11:16:21 -0400

    add devise to users; setup routes, and contollers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "home#index"
+
+  resources :home, only: [:index]
 
   namespace :admin do
     resources :customers, only: [:index]
+
+    devise_for :users, only: [:sessions], controllers: { sessions: "admin/users/sessions" }
   end
   
 end
